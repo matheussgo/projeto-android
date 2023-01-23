50739a6b1d61b45f84af3a9e14ecd83a284ee123
Matheus Soares Gonçalves <120135915+matheussgo@users.noreply.github.com>
2023-01-23 15:34:07 -0300

    Adicionei Javascript no projeto android
    Criei uma tag "img" de forma dinâmica, diretamente pelo javascript, para inserir a imagem "site em construção".

---
+++ newTag.js
@@ -0,0 +1,6 @@
+var img = document.createElement('img')
+var div = document.querySelector('article.emconstrucao')
+
+img.setAttribute('id', 'foto')
+img.setAttribute('src', 'imagens/site-em-construcao.jpg')
+div.appendChild(img)
